bd194df9df11c266ff8677b9acb55819c66b4392
xiaoxiehang <xiaoxiehang@gmail.com>
2014-07-11 10:23:04 +0800

    trip

--- zhuan/trip/js/pages/realwall.js
+++ zhuan/trip/js/pages/realwall.js
@@ -6,26 +6,26 @@ define(function(require,exports,module){
         var el = $(this),
             el_src = el.attr('src');
         var h = [
-            '<div class="m-winreal" id="slider">',
+            '<div class="m-winreal">',
                 '<div class="m-winreal-hd">',
-                    '<a href="#" class="fl"><i class="icon-r-realback"></i></a>',
-                    '<a href="#" class="u-realparise-btn"><i class="icon-r-realparise"></i> 123123</a>',
+                    '<a href="#" class="u-realback"></a>',
+                    '<a href="#" class="u-realparise">1</a>',
                 '</div>',
                 '<div class="m-winreal-bd"><img src="'+el_src+'" alt=""></div>',
                 '<div class="m-winreal-ft">',
                     '<div class="fl"><em>灰太狼</em><br><time>2014-05-16</time></div>',
-                    '<a href="javascript:;" class="u-realimg-btn"><i class="icon-r-realimg"></i>图片打印</a>',
+                    '<a href="javascript:;" class="u-realimg-btn">图片打印</a>',
                 '</div>',
             '</div>'
         ];
         $(document.body).append(h.join(''));
         
-    }).on('click','.icon-r-realback',function(){
+    }).on('click','.u-realback',function(){
         $('div.m-winreal').remove();
-    }).on('click','.u-realparise-btn',function(){
+    }).on('click','.u-realparise',function(){
         var num = parseInt($(this).text());
         num++;
-        $(this).html('<i class="icon-r-realparise"></i> '+num);
+        $(this).html(num);
     }).on('touchmove','.m-winreal',function(){
         return false;
     })
