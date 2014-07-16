36928bb1ad97ef596bb5136acde9cb88e696a6da
xiaoxiehang <xiaoxiehang@gmail.com>
2014-07-16 13:00:07 +0800

    trip

--- zhuan/trip/js/plugins/menu.js
+++ zhuan/trip/js/plugins/menu.js
@@ -2,21 +2,18 @@ define(function(require,exports,module){
     var zepto = require('zepto');    
     
     //显示导航
-    $(document.body).on('touchstart',function(e){
+    $(document.body).on('click',function(e){
         var el = $(e.target);
-        
-        if($('.g-nav-show').length){
+        if(!el.closest('.g-nav-show').length){
+            console.log(1);
             $('nav.g-nav').removeClass('g-nav-show');
-            if(!el.closest('.g-nav').length){
-                console.log(1);
-                return false;
-            }else{
-                console.log(2);
-            }
+            return false;
         }
-        if(el.hasClass('u-menu')){
-            $('.g-nav').addClass('g-nav-show');
-        }
+    })
+    
+    //导航显示
+    $(document.body).on('touchstart','.u-menu',function(){
+        $('.g-nav').addClass('g-nav-show');
     })
     
     //导航显示时，移动动作return
