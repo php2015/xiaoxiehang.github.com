feb43a2f47727a37df3cea440a672ab1083d33c2
xiaoxiehang <xiaoxiehang@gmail.com>
2014-07-23 12:28:39 +0800

    trip

--- zhuan/trip/js/pages/gambit.js
+++ zhuan/trip/js/pages/gambit.js
@@ -47,10 +47,20 @@ define(function(require,exports,module){
         if(el.hasClass('u-reply')){
             if(!$('.g-ft').find('.u-reply-txt').length){
                 $('.g-ft').append('<input class="u-reply-txt" type="text" placeholder="添加评论："><a href="javascript:;" class="u-reply-btn">发送</a>').find('.u-write-btn').hide();
+            }else{
+                $('.g-ft').find('.u-reply-txt,.u-reply-btn').show();
             }
+            $('.g-ft').find('.u-write-btn').hide();
             $('.u-reply-txt').data('dynamicID',el.parents('.m-dynamic').attr('id')).focus();
-            
         }
+        
+        if(el.closest('.g-bd').length){
+            var write_btn = $('.g-ft').find('.u-write-btn');
+//            if()
+//            $('.g-ft').find('.u-write-btn').show();
+        }
+    }).on('blur','.u-reply-txt',function(){
+        $('.g-ft').find('.u-write-btn').show().siblings().hide();
     })
     
     //添加评论
@@ -58,6 +68,7 @@ define(function(require,exports,module){
         var el = $(this),txt = el.siblings('.u-reply-txt'),dynamic_ID = txt.data('dynamicID');
         if(txt.val() == ''){
             txt.focus();
+            return false;
         }else{
             $('#'+dynamic_ID).find('.m-dynamic-list').append('<article class="m-dynamic-reply"><header class="m-dynamic-hd"><a class="m-dynamic-name">捷克</a><span class="m-dynamic-time">2014-05-30</span><a class="m-dynamic-face"><img src="http://img0.bdstatic.com/img/image/shouye/sjbztmxdg.jpg" alt="李米" width="40" height="40"></a></header><section class="m-dynamic-bd"><p>'+txt.val()+'</p></section></article>')
             
