d533125cc2239731cc671e0d2f6cf2d2dd975ff6
xiaoxiehang <xiaoxiehang@qq.com>
2014-12-01 10:31:35 +0800

    library
    Signed-off-by: xiaoxiehang <xiaoxiehang@qq.com>

--- zhuan/library/js/pages/book.js
+++ zhuan/library/js/pages/book.js
@@ -2,7 +2,7 @@ define(function(require,exports,module){
     var zepto = require('zepto');
     
     $(document.body).on('click',function(e){
-        var el = $(e.target);
+        var _this = $(this) , el = $(e.target);
         
         //锁定
         if(el.hasClass('j-lock')){
@@ -11,7 +11,13 @@ define(function(require,exports,module){
         
         //二维码
         if(el.closest('.j-code').length){
-            $(this).append('<div id="win-code" class="win-box"><div class="win-cont"><img src="#" width="" height="" alt=""><p>消耗点数：<em>1</em></p><p>使用规则：页面借阅扫描二维码，消耗点数1。</p></div></div>')
+            _this.append('<div id="win-code" class="win-box"><div class="win-cont"><img src="#" width="140" height="140" alt=""><p>消耗点数：<em>1</em></p><p>使用规则：页面借阅扫描二维码，消耗点数1。</p></div></div>');
+            
+            var h = $('#win-code').find('.win-cont').height();
+            
+            var winBox = $('.win-box');
+            var winCont = $('.win-cont');
+            winCont.css('margin-top',(winBox.height()-winCont.height())/2);
         }
         
     })
